c7ad855382f35f20110891f58f56446223a61999
Alex Corbi <a.corbi@gmail.com>
2016-05-23 08:51:12 +0200

    removing vendor folder ( should be generated on install with composer install)

--- vendor/byjg/webrequest/src/CurlException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ByJG\Util;
-
-class CurlException extends \Exception
-{
-    //put your code here
-}
--- vendor/byjg/webrequest/src/FileNotFoundException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ByJG\Util;
-
-class FileNotFoundException extends \Exception
-{
-    //put your code here
-}
