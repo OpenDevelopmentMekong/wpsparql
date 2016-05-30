63316d4f1f53b1ffd7eb0fe33b2e36ddde05b6cd
Alex Corbi <a.corbi@gmail.com>
2016-05-30 20:56:13 +0200

    Fixed incorrect priv key

--- _ci/install.sh
+++ _ci/install.sh
@@ -12,7 +12,7 @@ wget https://github.com/OpenDevelopmentMekong/odm-automation/archive/master.zip 
 unzip /tmp/odm_automation.zip -d /tmp/
 
 echo "decrypting private key and adding it key to ssh agent"
-openssl aes-256-cbc -K $encrypted_f5c2fe88ed3f_key -iv $encrypted_f5c2fe88ed3f_iv -in odm_tech_rsa.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_708a631cca83_key -iv $encrypted_708a631cca83_iv -in odm_tech_rsa.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
